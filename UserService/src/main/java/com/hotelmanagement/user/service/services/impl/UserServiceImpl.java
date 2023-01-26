532bf95ebd7431a68fb236b0a470c39777fec987
Ayush Tiwari <46418123+Rahul-71@users.noreply.github.com>
2023-01-26 16:54:13 +0530

    Getting hotel based in rating response for getUserByUserId controller

--- UserService/src/main/java/com/hotelmanagement/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/hotelmanagement/user/service/services/impl/UserServiceImpl.java
@@ -1,11 +1,18 @@
 package com.hotelmanagement.user.service.services.impl;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.hotelmanagement.user.service.entities.Hotel;
+import com.hotelmanagement.user.service.entities.Rating;
 import com.hotelmanagement.user.service.entities.User;
 import com.hotelmanagement.user.service.exceptions.ResourceNotFoundExceptions;
 import com.hotelmanagement.user.service.repositories.UserRepository;
@@ -16,6 +23,10 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RestTemplate restTemplate;
+    // @Autowired
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public User saveUser(User user) {
@@ -37,8 +48,31 @@ public class UserServiceImpl implements UserService {
     @Override
     public User getUser(String userId) {
         // to fetch user based in userId
-        return this.userRepository.findById(userId).orElseThrow(
+        User user = this.userRepository.findById(userId).orElseThrow(
                 () -> new ResourceNotFoundExceptions("User with given id: " + userId + " not found!!"));
+
+        // fetch rating of the above user from RATING-SERVICE
+        // http://localhost:8083/ratings/users/d4af178c-5438-4670-bf42-f6ff17e90c23
+        Rating[] ratingArray = this.restTemplate.getForObject(
+                "http://localhost:8083/ratings/users/" + user.getUserId(),
+                Rating[].class);
+
+        List<Rating> userRatings = Arrays.stream(ratingArray).toList();
+
+        List<Rating> ratings = userRatings.stream().map(rating -> {
+            // api call to hotel service
+            // http://localhost:8082/hotels/ae25f673-b728-4c5e-961b-f32750540b12
+            Hotel hotel = this.restTemplate
+                    .getForObject("http://localhost:8082/hotels/" + rating.getHotelId(), Hotel.class);
+
+            // set hotel to rating
+            rating.setHotel(hotel);
+            return rating;
+        }).collect(Collectors.toList());
+
+        user.setRatings(ratings);
+        logger.info("{} ", ratings);
+        return user;
     }
 
     @Override
